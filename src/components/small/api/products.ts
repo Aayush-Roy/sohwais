d6f4624bc68348e831037c83c508cde916814083
aayush <ar0671362@gmail.com>
2026-01-11 14:18:00 +0530

    removed axios

--- src/components/small/api/products.ts
+++ src/components/small/api/products.ts
@@ -1,4 +1,21 @@
-import axios from "axios";
+// import axios from "axios";
+// import { Product } from "../types/product";
+
+// interface ProductResponse {
+//   products: Product[];
+//   page: number;
+//   pages: number;
+//   total: number;
+// }
+
+// export const fetchProducts = async (): Promise<ProductResponse> => {
+//   // const res = await axios.get<ProductResponse>(
+//   //   "http://localhost:5000/api/products"
+//   // );
+//   const res = fetch("http://localhost:5000/api/products");
+//   return res.data;
+// };
+
 import { Product } from "../types/product";
 
 interface ProductResponse {
@@ -9,8 +26,12 @@ interface ProductResponse {
 }
 
 export const fetchProducts = async (): Promise<ProductResponse> => {
-  const res = await axios.get<ProductResponse>(
-    "http://localhost:5000/api/products"
-  );
-  return res.data;
+  const res = await fetch("http://localhost:5000/api/products");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch products");
+  }
+
+  const data: ProductResponse = await res.json();
+  return data;
 };
